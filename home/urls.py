42b0fd427964c8e3e1d449372925d01a0d99efef
Heulwen31 <52563541+Heulwen31@users.noreply.github.com>
2022-05-17 11:17:24 +0700

    show response chatbot

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('result/', views.save_commit, name='save_commit'),
+    path('response/', views.response_ans, name='response'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,10 +3,30 @@ from joblib import load
 
 from home.models import SentimentAnalysis
 
+
+import json 
+import numpy as np
+from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
+
+import colorama 
+colorama.init()
+from colorama import Fore, Style, Back
+
+import random
+import pickle
+
+# chatbot model 
+with open("intents.json") as file:
+    data = json.load(file)
+
+
+
+
 # Create your views here.
 # load model
 model = load('model\model.joblib')
-
+conversation = []
 # home page
 def home(request):
     return render(request, 'home\home.html')
@@ -25,3 +45,33 @@ def save_commit(request):
     sentiment = SentimentAnalysis(review=review, label=result)
     sentiment.save()
     return redirect('home')
+
+
+def response_ans(request):
+        # load trained model
+    model = keras.models.load_model('chat_model')
+
+    # load tokenizer object
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    # load label encoder object
+    with open('label_encoder.pickle', 'rb') as enc:
+        lbl_encoder = pickle.load(enc)
+
+    # parameters
+    max_len = 20
+
+    inp = request.GET['question']
+
+    result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                            truncating='post', maxlen=max_len))
+    tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+    for i in data['intents']:
+        if i['tag'] == tag:
+            print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , np.random.choice(i['responses']))
+            conversation.append(inp)
+            conversation.append(np.random.choice(i['responses']))
+            print(conversation)
+            return render(request, 'home\home.html', {'response': np.random.choice(i['responses'])})
